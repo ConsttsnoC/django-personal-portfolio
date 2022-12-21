6c0c579009d17a268666de1f4ab49f06e1ae9a37
ConsttsnoC <Constantaasamara@gmail.com>
2022-12-21 15:44:36 +0400

    Изменение иконок

--- blog/models.py
+++ blog/models.py
@@ -3,8 +3,10 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=200)
+    images = models.ImageField(upload_to='portfolio/images/',blank=True)
+    url = models.URLField(blank=True)
     descriptoin = models.TextField()
-    data = models.DateField()
+    data = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,3 @@ def all_blogs(request):
     return render(request, 'blog/all_blogs.html', {'blogs': blogs})
 
 
-def detail(request, blog_id):
-    blog = get_object_or_404(Blog, pk=blog_id)
-    return render(request, 'blog/detail.html', {'blog': blog})
