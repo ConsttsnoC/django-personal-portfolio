0ddd9451d66b9714023d39f8109491eb2baec986
ConsttsnoC <Constantaasamara@gmail.com>
2023-02-16 13:06:53 +0400

    fix(base.html): fix bags

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=200)
     images = models.ImageField(upload_to='portfolio/images/',blank=True)
     url = models.URLField(blank=True)
+    github_repo = models.URLField(blank=True)
     descriptoin = models.TextField()
     data = models.DateField(blank=True, null=True)
 
--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6+ignv2-nzx_)(@jb+(k@5*qrmph-qg-qsi$a+c7o0%x4i#49q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['www.gilmanov.net']
 
